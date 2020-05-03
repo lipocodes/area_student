7e16b6953bf91136e01a198674f4d3fcd8d55720
Lior Polak <toogle.app@gmail.com>
2020-05-03 15:13:41 +0300

    screen 9 : all data from FB ready

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -9,7 +9,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:areastudent/tools/firebase_methods.dart';
 import 'followers.dart';
 import 'following.dart';
-
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -19,26 +22,159 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   FirebaseMethods firebaseMethods = new FirebaseMethods();
 
+  String uid = "";
+  bool tempUid = false;
+  List<String> profileImages = [];
+  String name = "";
+  String country = "";
+  String region = "";
+  String subregion = "";
+  String locality = "";
+  String academicField = "";
+  String birthDate = "";
+  String age = "";
+  String aboutMe = "";
+  List<String> listFollowers = [];
+  int numFollowers = 0;
+  List<String> listFollowings = [];
+  int numFollowings = 0;
+  List<String> posts = [];
 
+  List<String> postsCreatorUid = [];
+  List<String> postsId = [];
+  List<String> postsCreationLatitude = [];
+  List<String> postsCreationLongitude = [];
+  List<String> postsCreationTime = [];
+  List<String> postsText = [];
+  List<List<String>> postsImages = [];
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
+  String gender = "";
 
+  Future<String> inputData() async {
+    try {
+      final FirebaseUser user = await FirebaseAuth.instance.currentUser();
+      String uid = user.uid.toString();
+      return uid;
+    } catch (e) {
+      this.tempUid = true;
+      return "";
+    }
   }
 
+  Future<void> retrieveUserData() async {
+    this.uid = await inputData();
+    if (this.tempUid == true) {
+      this.uid = "M0B7RtHW6zYOwkPhcqoHdigwEEs2";
+      this.tempUid = true;
+    }
 
-    void onPressedFollowingButton() {
+    final QuerySnapshot result = await Firestore.instance
+        .collection('userData')
+        .where('uid', isEqualTo: this.uid)
+        .getDocuments();
+    final List<DocumentSnapshot> snapshot = result.documents;
+
+    try {
+      List<dynamic> str1 = snapshot[0].data['profileImages'];
+      List<String> str2 = [];
+      for (int i = 0; i < str1.length; i++) {
+        str2.add(str1[i].toString());
+      }
+      this.profileImages = str2;
+
+      this.name =
+          snapshot[0].data['firstName'] + "  " + snapshot[0].data['lastName'];
+      this.gender = snapshot[0].data['gender'];
+      this.country = snapshot[0].data['country'];
+      this.region = snapshot[0].data['region'];
+      this.subregion = snapshot[0].data['region'];
+      this.locality = snapshot[0].data['locality'];
+      this.academicField = snapshot[0].data['academicField'];
+
+      var now = new DateTime.now();
+      var formatter = new DateFormat('yyyy');
+      int yearNow = int.parse(formatter.format(now));
+      this.birthDate = snapshot[0].data['birthDate'];
+      int yearBirthDate =
+          int.parse(this.birthDate.substring(this.birthDate.length - 4));
+      int age = yearNow - yearBirthDate;
+      this.age = age.toString();
+
+      this.aboutMe = snapshot[0].data['aboutMe'];
+
+      List<dynamic> str3 = snapshot[0].data['followers'];
+      List<String> str4 = [];
+      for (int i = 0; i < str3.length; i++) {
+        str4.add(str3[i].toString());
+      }
+      this.listFollowers = str4;
+      this.numFollowers = str4.length;
+
+      List<dynamic> str5 = snapshot[0].data['following'];
+      List<String> str6 = [];
+      for (int i = 0; i < str5.length; i++) {
+        str6.add(str5[i].toString());
+      }
+      this.listFollowings = str6;
+      this.numFollowings = str6.length;
+
+      List<dynamic> str7 = snapshot[0].data['posts'];
+      List<String> str8 = [];
+      for (int i = 0; i < str7.length; i++) {
+        str8.add(str7.toString());
+      }
+      this.posts = str8;
+      retrievePostsCurrentUser();
+    } catch (e) {
+      print("eeeeeeeeeeeeeeeeeeeeee= " + e.toString());
+    }
+  }
+
+  Future<void> retrievePostsCurrentUser() async {
+    this.uid = await inputData();
+    if (this.tempUid == true) {
+      this.uid = "M0B7RtHW6zYOwkPhcqoHdigwEEs2";
+      this.tempUid = true;
+    }
+
+    final QuerySnapshot result = await Firestore.instance
+        .collection('posts')
+        .where('creatorUid', isEqualTo: this.uid)
+        .getDocuments();
+    final List<DocumentSnapshot> snapshot = result.documents;
+
+    for (int i = 0; i < snapshot.length; i++) {
+      try {
+        this.postsId.add(snapshot[i].data['postId'].toString());
+        this.postsCreatorUid.add(snapshot[i].data['creatorUid'].toString());
+        this.postsCreationLatitude.add(snapshot[i].data['creationLatitude'].toString());
+        this.postsCreationLongitude.add(snapshot[i].data['creationLongitude'].toString());
+        this.postsCreationTime.add(snapshot[i].data['creationTime'].toString());
+        this.postsText.add(snapshot[i].data['text'].toString());
+        
+        List<dynamic> str9 = snapshot[i].data['images'];
+        List<String> str10 = [];
+        for(int i=0; i<str9.length; i++){
+          str10.add(str9[i].toString());
+        }
+        this.postsImages.add(str10);
+      
+      } catch (e) {
+        print("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee= " + e.toString());
+      }
+    }
+       
+  }
+
+  void onPressedFollowingButton() {
     Navigator.of(context).push(new CupertinoPageRoute(
-          builder: (BuildContext context) => new Followings()));
+        builder: (BuildContext context) => new Followings()));
   }
 
   void onPressedFollowersButton() {
     Navigator.of(context).push(new CupertinoPageRoute(
-          builder: (BuildContext context) => new Followers()));
+        builder: (BuildContext context) => new Followers()));
   }
-
 
   void onTappedSettingsButton(String choice) {
     if (choice == screen12Logout)
@@ -73,7 +209,6 @@ class _ProfileState extends State<Profile> {
     } else if (choice == screen12BlockedUsers) {
       Navigator.of(context).push(new CupertinoPageRoute(
           builder: (BuildContext context) => new BlockedUsers()));
-  
     } else if (choice == screen12Settings) {
       showDialog<void>(
         context: context,
@@ -100,8 +235,7 @@ class _ProfileState extends State<Profile> {
         builder: (BuildContext context) {
           return AlertDialog(
             title: Text('Edit Profile'),
-            content: const Text(
-                screen12EditProfileWarning),
+            content: const Text(screen12EditProfileWarning),
             actions: <Widget>[
               FlatButton(
                 child: Text('Ok'),
@@ -198,15 +332,20 @@ class _ProfileState extends State<Profile> {
     //exit(0);
     //Navigator.popUntil(context, ModalRoute.withName('/login'));
     Navigator.pushNamed(context, '/login');
-    
   }
 
+  Future<bool> _onBackPressed() async {
+    //signOut();
+    //Navigator.popUntil(context, ModalRoute.withName('/'));
+    return true;
+  }
 
-  Future<bool> _onBackPressed() async{
-   //signOut();
-   //Navigator.popUntil(context, ModalRoute.withName('/'));
-   return true;
-}
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    retrieveUserData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -223,6 +362,28 @@ class _ProfileState extends State<Profile> {
           ),
           backgroundColor: Colors.white,
         ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: 0, // this will be set when a new tab is tapped
+          items: [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.perm_identity, size: 30.0),
+              title: new Text('Profile'),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.group, size: 30.0),
+              title: new Text('Groups'),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home, size: 30.0), title: Text('Home')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_border, size: 30.0),
+                title: Text('Meet')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.chat_bubble_outline, size: 30.0),
+                title: Text('Chats'))
+          ],
+        ),
         body: Column(
           children: [
             Padding(
@@ -230,17 +391,16 @@ class _ProfileState extends State<Profile> {
                 child: new IconButton(
                     icon: Icon(Icons.settings),
                     onPressed: onPressedSettingsButton)),
-             Padding(
+            Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: new IconButton(
                     icon: Icon(Icons.favorite),
-                    onPressed: onPressedFollowersButton)),  
-
-             Padding(
+                    onPressed: onPressedFollowersButton)),
+            Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: new IconButton(
                     icon: Icon(Icons.local_drink),
-                    onPressed: onPressedFollowingButton)),               
+                    onPressed: onPressedFollowingButton)),
           ],
         ),
       ),
